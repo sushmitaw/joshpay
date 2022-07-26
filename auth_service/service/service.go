138972b2b11e1d1f924de5e3dfffe7ec09ba2e65
sushmitaw <sushmita.wable_js@external.swiggy.in>
2022-07-27 04:17:02 +0530

    base64 token

--- auth_service/service/service.go
+++ auth_service/service/service.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -15,24 +16,25 @@ func LoginService(req LoginRequest) (string, error) {
 var hmacSampleSecret []byte
 
 func Authenticate(req http.Request) error {
-	tokenString := req.Header.Get("Authorization")
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
+	token = req.Header.Get("Authorization")
+	tokenParams := strings.Split(token, " ")
 
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSampleSecret, nil
-	})
+	if tokenParams[0] != tokenPrefix {
+		return errors.New("invalid token prefix")
+	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims["id"], claims["name"])
-	} else {
+	decodedToken, err := decodeToken(tokenParams[1])
+	if err != nil {
 		return err
 	}
 
+	if string(decodedToken) != validUser {
+		return errors.New("invalid token user")
+	}
+
+	log.Print("Authenticated using base64 encoded token")
 	return nil
+
 }
 
 func AuthEncodedToken(token string) error {
@@ -51,4 +53,25 @@ func AuthEncodedToken(token string) error {
 		return errors.New("invalid token user")
 	}
 	return
+}
+
+func AuthJwtToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return hmacSampleSecret, nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["id"], claims["name"])
+	} else {
+		return err
+	}
+
+	return nil
+
 }
