5105d7f94c64b0089c00ef05ae6526891504cd74
sushmitaw <sushmita.wable_js@external.swiggy.in>
2022-08-10 09:07:50 +0530

    UPDATES

--- auth_service/service/service.go
+++ auth_service/service/service.go
@@ -16,24 +16,19 @@ func LoginService(req LoginRequest) (string, error) {
 var hmacSampleSecret = []byte("c2VjcmV0OktleQ==")
 
 func Authenticate(req http.Request) error {
-	tokenString := req.Header.Get("Authorization")
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSampleSecret, nil
-	})
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims["id"], claims["name"])
-	} else {
+	token := req.Header.Get("Authorization")
+	tokenParams := strings.Split(token, " ")
+	if tokenParams[0] != tokenPrefix {
+		return errors.New("invalid token prefix")
+	}
+	decodedToken, err := decodeToken(tokenParams[1])
+	if err != nil {
 		return err
 	}
-	log.Print("Authenticated using  jwt token")
-
+	if string(decodedToken) != validUser {
+		return errors.New("invalid token user")
+	}
+	log.Print("Authenticated using base64 encoded token")
 	return nil
 
 }
--- payment_service/service/service.go
+++ payment_service/service/service.go
@@ -1,5 +1,22 @@
 package service
 
+import (
+	"github.com/sushmitaw/joshpay/fraud_detection_service"
+)
+
 func InitiatePaymentService(req PaymentRequest) error {
-     return nil
+	//simulate payment success
+	go checkIfSuspicious(req)
+	return nil
+}
+
+func checkIfSuspicious(req PaymentRequest) {
+	//get history from database
+	history := []fraud_detection_service.PaymentRequest{}
+
+	isSuspicious := fraud_detection_service.CheckTransaction(req.Amount, req.CreditCardToken, history)
+	if isSuspicious {
+		//add in db for further investigation
+	}
+
 }
